6383c7d86c5f12d4b021ad761d10778aaabb2400
Adam Hawkin <adam.hawkin@protonmail.com>
2018-01-20 17:20:59 +0000

    Updated deploy script to use promise

--- deploy.js
+++ deploy.js
@@ -2,16 +2,14 @@ const fs = require('fs-extra');
 const ghpages = require('gh-pages');
 const path = require('path');
 
-async function copyFiles() {
-  try {
-    await fs.copy(path.resolve(__dirname, './index.html'), './dist/index.html');
-    await fs.copy(path.resolve(__dirname, './favicon.ico'), './dist/favicon.ico');
-    await fs.copy(path.resolve(__dirname, './manifest.json'), './dist/manifest.json');
-    await fs.copy(path.resolve(__dirname, './assets/'), './dist/assets');
-  } catch (err) {
-    console.error(err);
-  }
-}
+const copyFiles = () => {
+  const index = fs.copy(path.resolve(__dirname, './index.html'), './dist/index.html');
+  const favicon = fs.copy(path.resolve(__dirname, './favicon.ico'), './dist/favicon.ico');
+  const manifest = fs.copy(path.resolve(__dirname, './manifest.json'), './dist/manifest.json');
+  const assets = fs.copy(path.resolve(__dirname, './assets/'), './dist/assets');
+
+  return Promise.all([index, favicon, manifest, assets]);
+};
 
 const deploy = () => {
   copyFiles().then(() => {
