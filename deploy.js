dc857e86af4eab8898ebd40d808715d9aa6873d7
Adam Hawkin <adam.hawkin@protonmail.com>
2018-01-20 15:21:12 +0000

    init gh-pages branch

--- deploy.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: 'https://example.com/other/repo.git',
-});
--- src/js/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
-
-import '../sass/main.scss';
-
-render(<App />, document.getElementById('app'));
