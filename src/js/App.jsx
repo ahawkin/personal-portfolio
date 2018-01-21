a513573d9a68bf79f1889eedca1bbbdf4a40eced
Adam Hawkin <adam.hawkin@protonmail.com>
2018-01-21 13:43:32 +0000

    Removed gap

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
-import Hero from './components/Hero/Hero';
-import Footer from './components/Footer/Footer';
 import Projects from './containers/Projects/Projects';
 import About from './containers/About/About';
 import Navigation from './containers/Navigation/Navigation';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 
 const App = () => (
   <div>
