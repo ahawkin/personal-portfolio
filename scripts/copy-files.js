cbf9885f4ddfdc0de58da6804524f0d5c6f60e77
Adam Hawkin <adam.hawkin@protonmail.com>
2019-12-08 22:09:20 +0000

    Copy robots.txt

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -6,6 +6,7 @@ const copyFiles = () => {
 
   const favicon = fs.copy(path.resolve(__dirname, '../assets/favicon.ico'), path.resolve(__dirname, '../dist/favicon.ico'));
   const manifest = fs.copy(path.resolve(__dirname, '../assets/manifest.json'), path.resolve(__dirname, '../dist/manifest.json'));
+  const robots = fs.copy(path.resolve(__dirname, '../assets/manifest.json'), path.resolve(__dirname, '../dist/robots.txt'));
   const cname = fs.copy(path.resolve(__dirname, '../config/CNAME'), path.resolve(__dirname, '../dist/CNAME'));
   const files = fs.copy(path.resolve(__dirname, '../assets/files'), path.resolve(__dirname, '../dist/assets/files'));
   const images = fs.copy(path.resolve(__dirname, '../assets/img'), path.resolve(__dirname, '../dist/assets/img'));
@@ -13,6 +14,7 @@ const copyFiles = () => {
   return Promise.all([
     favicon,
     manifest,
+    robots,
     cname,
     files,
     images
