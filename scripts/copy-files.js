99d7675518415e347e834799d39a2809fa470b39
Adam Hawkin <adam.hawkin@protonmail.com>
2019-12-08 22:15:52 +0000

    Update copy script

--- scripts/copy-files.js
+++ scripts/copy-files.js
@@ -6,7 +6,7 @@ const copyFiles = () => {
 
   const favicon = fs.copy(path.resolve(__dirname, '../assets/favicon.ico'), path.resolve(__dirname, '../dist/favicon.ico'));
   const manifest = fs.copy(path.resolve(__dirname, '../assets/manifest.json'), path.resolve(__dirname, '../dist/manifest.json'));
-  const robots = fs.copy(path.resolve(__dirname, '../assets/manifest.json'), path.resolve(__dirname, '../dist/robots.txt'));
+  const robots = fs.copy(path.resolve(__dirname, '../assets/robots.txt'), path.resolve(__dirname, '../dist/robots.txt'));
   const cname = fs.copy(path.resolve(__dirname, '../config/CNAME'), path.resolve(__dirname, '../dist/CNAME'));
   const files = fs.copy(path.resolve(__dirname, '../assets/files'), path.resolve(__dirname, '../dist/assets/files'));
   const images = fs.copy(path.resolve(__dirname, '../assets/img'), path.resolve(__dirname, '../dist/assets/img'));
